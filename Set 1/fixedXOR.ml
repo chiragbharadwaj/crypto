e944ac73bde3186ce382d6ef14a91a69eb90ec0d
Chirag Bharadwaj <cb625@cornell.edu>
2015-11-04 18:47:07 -0500

    Added some comments

--- Set 1/fixedXOR.ml
+++ Set 1/fixedXOR.ml
@@ -1,18 +1,34 @@
+(* The only thing that is publicly visible is the xor function, which is the
+ * type of abstraction we are going for: the implementaiton is NOT visible. *)
 module type XOR = sig
 
+	(* [xor (s1, s2)] returns a string that corresponds to the logical XOR of
+	 *  the two input strings s1 and s2. Requires: s1 and s2 are hex-encoded.
+	 *
+	 *  - [s1] is a hex-encoded string.
+	 *  - [s2] is another hex-encoded string that s1 is logical XOR'ed with.
+	 *)
 	val xor : string * string -> string
 
 end
 
+(* The actual internal implementation of the XOR signature. These helper
+ * functions are not seen from outside, and are not usable out of this scope. *)
 module FixedXOR : XOR = struct
 
+	(* Simple type to encode binary numbers, useful in limiting conversion. *)
 	type binary = Zero | One
 
+	(* [to_binary i] converts an integer i to its corresponding binary construct
+	 *  by invoking the constructor. Requires: i = 0 or i = 1.
+	 *
+	 *  - [i] is an integer representation of a binary number.
+	 *)
 	let to_binary (i : int) : binary =
 		match i with
 		| 0 -> Zero
 		| 1 -> One
-		| _ -> failwith "15: This should not happen."
+		| _ -> failwith "19: This should not happen." (* Not a binary number! *)
 
 	let to_decimal (b : binary) : int =
 		match b with
@@ -30,11 +46,12 @@ module FixedXOR : XOR = struct
 		| 2 -> Zero :: Zero :: b_list
 		| 3 -> Zero :: b_list
 		| 4 -> b_list
-		| _ -> failwith "33: This should not happen."
+		| _ -> failwith "37: This should not happen."
 
 	let ungroup (c : char) : binary list =
 		let rec bin (i : int) : binary list =
-			if (i = i mod 2) then (to_binary i) :: [] else bin (i / 2) @ [to_binary (i mod 2)]
+			if (i = i mod 2) then (to_binary i) :: []
+				else bin (i / 2) @ [to_binary (i mod 2)]
 		in
 		let hex (ch : char) : int =	
 			match ch with
@@ -46,12 +63,13 @@ module FixedXOR : XOR = struct
 			| 'f' -> 15
 			| x when abs (Char.code x - Char.code '0') <= 9 ->
 				int_of_string (Char.escaped ch)
-			| _   -> failwith "49: This should not happen."
+			| _   -> failwith "54: This should not happen."
 		in
 			zero_pad (bin (hex c))
 
 	let regroup (b_list : binary list) : char =
-		let num = List.fold_left (fun acc x -> (to_decimal x) + (2 * acc)) 0 b_list in
+		let num =
+			List.fold_left (fun acc x -> (to_decimal x) + (2 * acc)) 0 b_list in
 		let map_over (numerical : int) : char = (* Map to corresponding char *)
 			match numerical with
 			| 10 -> 'a'
@@ -61,7 +79,7 @@ module FixedXOR : XOR = struct
 			| 14 -> 'e'
 			| 15 -> 'f'
 			| x when abs x <= 9 -> String.get (string_of_int x) 0
-			| _ -> failwith "64: This should not happen."
+			| _ -> failwith "69: This should not happen."
 		in
 			map_over num
 
@@ -93,17 +111,25 @@ module FixedXOR : XOR = struct
 		| 1 -> (String.get s 0) :: [] (* Needed because sub from 1 undefined. *)
 		| _ -> (String.get s 0) :: char_of_string ( String.sub s 1 (len - 1) )
 
+	(* [xor (s1, s2)] returns a string that corresponds to the logical XOR of
+	 *  the two input strings s1 and s2. Requires: s1 and s2 are hex-encoded.
+	 *
+	 *  - [s1] is a hex-encoded string.
+	 *  - [s2] is another hex-encoded string that s1 is logical XOR'ed with.
+	 *)
 	let xor ((s1, s2) : string * string) : string =
 		let bl1 = List.flatten (List.map ungroup (char_of_string s1)) in
 		let bl2 = List.flatten (List.map ungroup (char_of_string s2)) in
 		let b_list =
-			try List.fold_left2 (fun acc b1 b2 -> (acc @ [bitxor (b1, b2)])) [] bl1 bl2
-			with | _ -> failwith "101: This should not happen."
+			try List.fold_left2
+				(fun acc b1 b2 -> (acc @ [bitxor (b1, b2)]))[] bl1 bl2
+			with | _ -> failwith "106: This should not happen."
 		in (* List.fold_left2 will catch uneven-length error, thankfully! *)
 		let rec recover (bl : binary list) : char list =
 			match bl with
 			| [] -> []
-			| b1 :: b2 :: b3 :: b4 :: rest -> (regroup (b1 :: b2 :: b3 :: b4 :: [])) :: recover rest
+			| b1 :: b2 :: b3 :: b4 :: rest ->
+				(regroup (b1 :: b2 :: b3 :: b4 :: [])) :: recover rest
 			| b1 :: b2 :: b3 :: [] as b -> (regroup (zero_pad b)) :: []
 			| b1 :: b2 :: [] as b -> (regroup (zero_pad b)) :: []
 			| b1 :: [] as b -> (regroup (zero_pad b)) :: []
